369d7137e61dfda7c17b3dfd4319819241890266
yash-vaghasiya <yashvaghasiya602@gmail.com>
2023-06-05 17:42:39 +0530

    Multipal array in object , MapMethod, ForEach method, Reduse right method

---
+++ java script/05-06-2023/multi-pal-array-object.js
@@ -0,0 +1,15 @@
+const data = [
+    {
+        "name" : "chiragbhai",
+        "City" : {"Surname": "chovatiya",
+        "city": "junagadh",
+    }
+    },
+    {
+        "name" : "yash",
+        "Surname": "vaghasiya",
+        "city": "rajkot",   
+    }
+];
+
+console.log(data[0].City.city);
